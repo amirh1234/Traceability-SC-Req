4dc359c108bf499d3f25c8f705110d7e18116a46
amirh1234 <41575256+amirh1234@users.noreply.github.com>
2018-07-24 11:25:28 +0430

    Add files via upload

---
+++ String Distance/String Distance/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace String_Distance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Run = new System.Windows.Forms.Button();
+            this.File = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(95, 102);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.TabIndex = 0;
+            this.Run.Text = "Run";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // File
+            // 
+            this.File.Location = new System.Drawing.Point(95, 62);
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(75, 23);
+            this.File.TabIndex = 1;
+            this.File.Text = "File";
+            this.File.UseVisualStyleBackColor = true;
+            this.File.Click += new System.EventHandler(this.File_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.File);
+            this.Controls.Add(this.Run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.Button File;
+    }
+}
+
---
+++ String Distance/String Distance/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using SCICT.NLP.Utility.StringDistance;
+namespace String_Distance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       // String_Distance
+       
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            SCICT.NLP.Utility.StringDistance.StringDistanceLayout layout = new SCICT.NLP.Utility.StringDistance.StringDistanceLayout();
+            double cosine = layout.GetWordSimilarity("reward", "drawer", SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.Cosine);
+            double damerauLevenestain = layout.GetWordSimilarity("reward", "drawer", SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.DamerauLevenestain);
+            double gnuLevenesain = layout.GetWordSimilarity("reward", "drawer", SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.GNULevenesain);
+            double hamming = layout.GetWordSimilarity("reward", "drawer", SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.Hamming);
+            double jaroWinkler = layout.GetWordSimilarity("reward", "drawer", SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.JaroWinkler);
+            double kashefi = layout.GetWordSimilarity("reward", "drawer", SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.Kashefi);
+            double levenestain = layout.GetWordSimilarity("reward", "drawer", SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.Levenestain);
+            double needleman = layout.GetWordSimilarity("reward", "drawer", SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.Needleman);
+            double wagnerFischer = layout.GetWordSimilarity("reward", "drawer", SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.WagnerFischer); 
+        }
+
+        private void File_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader("E:\\Test.txt");
+            while (reader.Read() != -1)
+            {
+                string tmp = reader.ReadLine();
+                string[] fiels = tmp.Split(',');
+                StringDistanceLayout layout = new StringDistanceLayout();
+                double cosine = layout.GetWordSimilarity(fiels[0], fiels[1], SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.Cosine);
+                double damerauLevenestain = layout.GetWordSimilarity(fiels[0], fiels[1], SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.DamerauLevenestain);
+                double gnuLevenesain = layout.GetWordSimilarity(fiels[0], fiels[1], SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.GNULevenesain);
+                double hamming = layout.GetWordSimilarity(fiels[0], fiels[1], SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.Hamming);
+                double jaroWinkler = layout.GetWordSimilarity(fiels[0], fiels[1], SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.JaroWinkler);
+                double kashefi = layout.GetWordSimilarity(fiels[0], fiels[1], SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.Kashefi);
+                double levenestain = layout.GetWordSimilarity(fiels[0], fiels[1], SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.Levenestain);
+                double needleman = layout.GetWordSimilarity(fiels[0], fiels[1], SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.Needleman);
+                double wagnerFischer = layout.GetWordSimilarity(fiels[0], fiels[1], SCICT.NLP.Utility.StringDistance.StringDistanceAlgorithm.WagnerFischer); 
+            }
+            reader.Close();
+        }
+
+    }
+}
